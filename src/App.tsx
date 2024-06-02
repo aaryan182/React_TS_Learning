453d348709b5ff9821550d980c603b9a53ca4406
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-06-02 10:38:23 +0530

    useContext hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Container } from "./components/Container";
-import { Input } from "./components/Input";
+import { UserContextProvider } from "./components/context/UserContext";
+import { User } from "./components/context/User";
 
 function App() {
   // const personName = {
@@ -36,7 +35,13 @@ function App() {
     <Greet name="Aaryan" messageCount={18} isLoggedIn={true}/> */}
     {/* <Button handleClick={(event,id) => console.log('Button Clicked' , event, id)} />
     <Input value="" handleChange={(event) => console.log(event)} /> */}
-    <Container styles={{border: '1px solid black', padding: '1rem'}} />
+    {/* <Container styles={{border: '1px solid black', padding: '1rem'}} /> */}
+    {/* <ThemeContextProvider>
+      <Box />
+    </ThemeContextProvider> */}
+    <UserContextProvider>
+      <User />
+    </UserContextProvider>
   </div>
 }
 
